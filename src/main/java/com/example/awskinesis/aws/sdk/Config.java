b3e4f31b7b5555df629eeebd88cf9ad3759a9d1f
Gustavo Adolfo Rodriguez <1432287+garodriguezlp@users.noreply.github.com>
2022-09-06 09:18:26 -0500

    Improve aws creds management

--- src/main/java/com/example/awskinesis/aws/sdk/Config.java
+++ src/main/java/com/example/awskinesis/aws/sdk/Config.java
@@ -1,7 +1,10 @@
 package com.example.awskinesis.aws.sdk;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.kinesis.AmazonKinesis;
 import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +13,18 @@ import static com.amazonaws.regions.Regions.US_EAST_1;
 @Configuration
 public class Config {
 
+    @Value("${aws.access-key-id}")
+    private String accessKeyId;
+
+    @Value("${aws.secret-key}")
+    private String secretKey;
+
     @Bean
     public AmazonKinesis awsKinesis() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKeyId, secretKey);
         return AmazonKinesisClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .withRegion(US_EAST_1)
                 .build();
     }
-
-
 }
